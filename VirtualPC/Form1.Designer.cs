046733e66479c3922b99e8781721d7f41b51bf7d
Daniel Loera <loeraso@outlook.com>
2019-01-15 15:08:16 -0600

    Primeros cambios

--- VirtualPC/Form1.Designer.cs
+++ VirtualPC/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace VirtualPC
 {
-    partial class Form1
+    partial class VirtualPC
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // VirtualPC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "VirtualPC";
+            this.Text = "VirtualPC";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- VirtualPC/Form1.cs
+++ VirtualPC/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace VirtualPC
 {
-    public partial class Form1 : Form
+    public partial class VirtualPC : Form
     {
-        public Form1()
+        public VirtualPC()
         {
             InitializeComponent();
         }
