21c08280dcaa79d0f483d2cc2bf37be8bf93cd05
Daniel Loera <loeraso@outlook.com>
2019-01-15 16:54:47 -0600

    Time Tester

--- VirtualPC/Form1.Designer.cs
+++ VirtualPC/Form1.Designer.cs
@@ -28,20 +28,295 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox50c = new System.Windows.Forms.TextBox();
+            this.lbl50c = new System.Windows.Forms.Label();
+            this.Timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label = new System.Windows.Forms.Label();
+            this.textBox1p = new System.Windows.Forms.TextBox();
+            this.lbl_TinC = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox5p = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2p = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox100p = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox50p = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox20p = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox10p = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox500p = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox200p = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cuentas";
+            // 
+            // textBox50c
+            // 
+            this.textBox50c.Location = new System.Drawing.Point(13, 106);
+            this.textBox50c.Name = "textBox50c";
+            this.textBox50c.Size = new System.Drawing.Size(57, 20);
+            this.textBox50c.TabIndex = 1;
+            this.textBox50c.Text = "0";
+            this.textBox50c.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // lbl50c
+            // 
+            this.lbl50c.AutoSize = true;
+            this.lbl50c.Location = new System.Drawing.Point(13, 87);
+            this.lbl50c.Name = "lbl50c";
+            this.lbl50c.Size = new System.Drawing.Size(34, 13);
+            this.lbl50c.TabIndex = 2;
+            this.lbl50c.Text = "$0.50";
+            // 
+            // Timer1
+            // 
+            this.Timer1.Enabled = true;
+            this.Timer1.Interval = 1000;
+            this.Timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(73, 87);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(19, 13);
+            this.label.TabIndex = 4;
+            this.label.Text = "$1";
+            // 
+            // textBox1p
+            // 
+            this.textBox1p.Location = new System.Drawing.Point(76, 106);
+            this.textBox1p.Name = "textBox1p";
+            this.textBox1p.Size = new System.Drawing.Size(57, 20);
+            this.textBox1p.TabIndex = 3;
+            this.textBox1p.Text = "0";
+            this.textBox1p.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // lbl_TinC
+            // 
+            this.lbl_TinC.AutoSize = true;
+            this.lbl_TinC.Location = new System.Drawing.Point(833, 109);
+            this.lbl_TinC.Name = "lbl_TinC";
+            this.lbl_TinC.Size = new System.Drawing.Size(69, 13);
+            this.lbl_TinC.TabIndex = 5;
+            this.lbl_TinC.Text = "Total en caja";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(199, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "$5";
+            // 
+            // textBox5p
+            // 
+            this.textBox5p.Location = new System.Drawing.Point(202, 106);
+            this.textBox5p.Name = "textBox5p";
+            this.textBox5p.Size = new System.Drawing.Size(57, 20);
+            this.textBox5p.TabIndex = 8;
+            this.textBox5p.Text = "0";
+            this.textBox5p.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(136, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "$2";
+            // 
+            // textBox2p
+            // 
+            this.textBox2p.Location = new System.Drawing.Point(139, 106);
+            this.textBox2p.Name = "textBox2p";
+            this.textBox2p.Size = new System.Drawing.Size(57, 20);
+            this.textBox2p.TabIndex = 6;
+            this.textBox2p.Text = "0";
+            this.textBox2p.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(451, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "$100";
+            // 
+            // textBox100p
+            // 
+            this.textBox100p.Location = new System.Drawing.Point(454, 106);
+            this.textBox100p.Name = "textBox100p";
+            this.textBox100p.Size = new System.Drawing.Size(57, 20);
+            this.textBox100p.TabIndex = 16;
+            this.textBox100p.Text = "0";
+            this.textBox100p.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(388, 87);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(25, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "$50";
+            // 
+            // textBox50p
+            // 
+            this.textBox50p.Location = new System.Drawing.Point(391, 106);
+            this.textBox50p.Name = "textBox50p";
+            this.textBox50p.Size = new System.Drawing.Size(57, 20);
+            this.textBox50p.TabIndex = 14;
+            this.textBox50p.Text = "0";
+            this.textBox50p.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(325, 87);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(25, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "$20";
+            // 
+            // textBox20p
+            // 
+            this.textBox20p.Location = new System.Drawing.Point(328, 106);
+            this.textBox20p.Name = "textBox20p";
+            this.textBox20p.Size = new System.Drawing.Size(57, 20);
+            this.textBox20p.TabIndex = 12;
+            this.textBox20p.Text = "0";
+            this.textBox20p.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(265, 87);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(25, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "$10";
+            // 
+            // textBox10p
+            // 
+            this.textBox10p.Location = new System.Drawing.Point(265, 106);
+            this.textBox10p.Name = "textBox10p";
+            this.textBox10p.Size = new System.Drawing.Size(57, 20);
+            this.textBox10p.TabIndex = 10;
+            this.textBox10p.Text = "0";
+            this.textBox10p.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(577, 87);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(31, 13);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "$500";
+            // 
+            // textBox500p
+            // 
+            this.textBox500p.Location = new System.Drawing.Point(580, 106);
+            this.textBox500p.Name = "textBox500p";
+            this.textBox500p.Size = new System.Drawing.Size(57, 20);
+            this.textBox500p.TabIndex = 20;
+            this.textBox500p.Text = "0";
+            this.textBox500p.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(514, 87);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(31, 13);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "$200";
+            // 
+            // textBox200p
+            // 
+            this.textBox200p.Location = new System.Drawing.Point(517, 106);
+            this.textBox200p.Name = "textBox200p";
+            this.textBox200p.Size = new System.Drawing.Size(57, 20);
+            this.textBox200p.TabIndex = 18;
+            this.textBox200p.Text = "0";
+            this.textBox200p.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // VirtualPC
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(914, 450);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox500p);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox200p);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox100p);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox50p);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox20p);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox10p);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox5p);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2p);
+            this.Controls.Add(this.lbl_TinC);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.textBox1p);
+            this.Controls.Add(this.lbl50c);
+            this.Controls.Add(this.textBox50c);
+            this.Controls.Add(this.label1);
             this.Name = "VirtualPC";
             this.Text = "VirtualPC";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox50c;
+        private System.Windows.Forms.Label lbl50c;
+        private System.Windows.Forms.Timer Timer1;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.TextBox textBox1p;
+        private System.Windows.Forms.Label lbl_TinC;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox5p;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2p;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox100p;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox50p;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox20p;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox10p;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox500p;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox200p;
     }
 }
 
--- VirtualPC/Form1.cs
+++ VirtualPC/Form1.cs
@@ -16,5 +16,21 @@ namespace VirtualPC
         {
             InitializeComponent();
         }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            double Var50c = Convert.ToDouble(textBox50c.Text);
+            double Var1p = Convert.ToDouble(textBox1p.Text);
+            double Var2p = Convert.ToDouble(textBox2p.Text);
+            double Var5p = Convert.ToDouble(textBox5p.Text);
+            double Var10p = Convert.ToDouble(textBox10p.Text);
+            double Var20p = Convert.ToDouble(textBox20p.Text);
+            double Var50p = Convert.ToDouble(textBox50p.Text);
+            double Var100p = Convert.ToDouble(textBox100p.Text);
+            double Var200p = Convert.ToDouble(textBox200p.Text);
+            double Var500p = Convert.ToDouble(textBox500p.Text);
+            double Tot_Caja = (Var50c*0.5)+Var1p+(Var2p*2) + (Var5p * 5) + (Var10p * 10) + (Var20p * 20) + (Var50p * 50) + (Var100p * 100) + (Var200p * 200) + (Var500p * 500);
+            lbl_TinC.Text = Tot_Caja.ToString();
+        }
     }
 }
